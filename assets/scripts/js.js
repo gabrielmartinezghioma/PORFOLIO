abf65abcc6bac804b9c4dd66abd442436af67e14
GHIOMA <martinezgabrielalejandro4@gmail.com>
2022-12-25 17:25:35 -0300

    addvances

--- assets/scripts/js.js
+++ assets/scripts/js.js
@@ -1,17 +1,9 @@
-
-// // incorporo contenido html
-// const bodyHtml = document.querySelector('.body');
-// bodyHtml.innerHTML+='<h2>holu</h2>'
-
-// //cambio de clase
-// bodyHtml.classList.contains('aa')
-
 const parallax = document.querySelector('.section__article--parallax');
 const front = document.querySelector('.article--front');
 const back = document.querySelector('.article--back');
 
-const sFront = 150;
-const sBack = 400;
+const sFront = 100;
+const sBack = 300;
 
 parallax.addEventListener('mousemove', e => {
     const x = e.clientX;
