a4a8df9c127140c3bee83d8efba86ec8ed325401
GHIOMA <martinezgabrielalejandro4@gmail.com>
2022-12-29 19:19:52 -0300

    advances

--- assets/scripts/js.js
+++ assets/scripts/js.js
@@ -1,16 +1,31 @@
+let articleUlLi = document.querySelectorAll('.article__ul--li')
+
+let articleDivBx = document.querySelectorAll('.article__div--Bx')
 
 
+for (let i = 0; i < articleDivBx.length; i++) {
+    articleUlLi[i].addEventListener('mouseover', function () {
+        for (let i = 0; i < articleDivBx.length; i++) {
+            articleDivBx[i].className = 'article__div--Bx'
+        }
+        document.getElementById(this.dataset.id).className='article__div--Bx article__ul--active'
 
+        for (let i = 0; i < articleUlLi.length; i++) {
+            articleUlLi[i].className = 'article__ul--li'
+        }
+        this.className='article__ul--li article__ul--active'
+    })
+}
 
 
 // slider auto
-let counter=1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 4 ){
-        counter = 1;
-    }
-},8000);
+let counter = 1;
+    setInterval(function () {
+        document.getElementById('radio' + counter).checked = true;
+        counter++;
+        if (counter > 4) {
+            counter = 1;
+        }
+    }, 8000);
 
 
