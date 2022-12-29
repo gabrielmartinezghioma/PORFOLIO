74033e6d7bd8afb7800f9ad2ae1b550b9d111403
GHIOMA <martinezgabrielalejandro4@gmail.com>
2022-12-29 09:26:05 -0300

    advances

--- assets/scripts/js.js
+++ assets/scripts/js.js
@@ -1,27 +1,3 @@
-const parallax = document.querySelector('.section__article--parallax');
-const front = document.querySelector('.article--front');
-const back = document.querySelector('.article--back');
-
-const sFront = 150;
-const sBack = 400;
-
-parallax.addEventListener('mousemove', e => {
-    const x = e.clientX;
-    const y = e.clientY;
-
-    front.style.transform = `
-    translate(
-        ${x / sFront}%, 
-        ${y / sFront}%
-    )`;
-
-    back.style.transform = `
-    translate(
-        ${x / sBack}%, 
-        ${y / sBack}%
-     )`;
-});
-
 let counter=1;
 setInterval(function(){
     document.getElementById('radio' + counter).checked = true;
