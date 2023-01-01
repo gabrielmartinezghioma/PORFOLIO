a1ae9aed1cf6bf741232f5d181c9d5752b3d83b0
GHIOMA <martinezgabrielalejandro4@gmail.com>
2023-01-01 11:48:50 -0300

    advances

--- assets/scripts/js.js
+++ assets/scripts/js.js
@@ -1,5 +1,25 @@
-let articleUlLi = document.querySelectorAll('.article__ul--li')
+//menu
+let navDivMenuResponsive = document.querySelector('.nav__div--menuResponsive');
 
+let navUl = document.querySelector('.nav__ul');
+
+
+navDivMenuResponsive.addEventListener('click', () => {
+    navUl.classList.toggle('nav__ul--toggle')
+
+})
+
+
+
+
+
+
+
+
+
+
+//header
+let articleUlLi = document.querySelectorAll('.article__ul--li')
 let articleDivBx = document.querySelectorAll('.article__div--Bx')
 
 
@@ -8,24 +28,24 @@ for (let i = 0; i < articleDivBx.length; i++) {
         for (let i = 0; i < articleDivBx.length; i++) {
             articleDivBx[i].className = 'article__div--Bx'
         }
-        document.getElementById(this.dataset.id).className='article__div--Bx article__ul--active'
+        document.getElementById(this.dataset.id).className = 'article__div--Bx article__ul--active'
 
         for (let i = 0; i < articleUlLi.length; i++) {
             articleUlLi[i].className = 'article__ul--li'
         }
-        this.className='article__ul--li article__ul--active'
+        this.className = 'article__ul--li article__ul--active'
     })
 }
 
 
 // slider auto
 let counter = 1;
-    setInterval(function () {
-        document.getElementById('radio' + counter).checked = true;
-        counter++;
-        if (counter > 4) {
-            counter = 1;
-        }
-    }, 8000);
+setInterval(function () {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 8000);
 
 
